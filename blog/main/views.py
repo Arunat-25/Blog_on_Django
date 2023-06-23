e62c148ccf5b45baf495f08582fb6312717ebda7
Arunat-25 <128986470+Arunat-25@users.noreply.github.com>
2023-06-23 17:59:58 +0700

    first

---
+++ blog/main/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+#from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
+def post_list(request):
+    post_lists = Post.published.all()
+    # постраничная разбивка с 3 постами на старнице
+    paginator = Paginator(post_lists, 4)
+    page_number = request.GET.get('page')  # если нет по такому юрл постарничной разбивки, переносит на 1 стр
+    #posts = paginator.page(page_number)  #вызывая метода page получаем обьекты для желаемой страницы
+    try:
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page('1')
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'main/post/list.html',
+                  {'posts': posts})
+
+#первый способ если база данных не содержит значение по такому id
+#def post_detail(request, id):
+#    try:
+#       post = Post.published.get(id=id)
+#    except Post.DoesNotExist:
+#        raise Http404('No post found ')
+#
+#    return render(request,
+#                  'main/post/detail.html',
+#                  {'post': post})
+
+# Второй способ если база данных не содержит значение по такому id
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day,)
+    return render(request,
+                  'main/post/detail.html',
+                  {'post': post})
+
+
+
+def create(request):
+    pass
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request,
+                  'main/post/share.html',
+                  {'post': post,
+                   'form': form})
